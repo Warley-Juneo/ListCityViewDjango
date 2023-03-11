3ddd23a2439e1aa4e784f0468bac77359024e6ca
Warley-Juneo <warleyjuneo111@gmail.com>
2023-03-11 18:59:38 -0300

    feat: search list completed

--- findCity/forms.py
+++ findCity/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class CitySearchForm(forms.Form):
-	search_termmmm = forms.CharField(label='Search')
+	search_term = forms.CharField(label='Search')
--- findCity/views.py
+++ findCity/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from django.views.generic.edit import FormView
 from .models import City
 from .forms import CitySearchForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
-from django.urls import reverse_lazy
+from django.views import View
 
 class CityListView(ListView):
 	model = City
 	template_name = 'city_list.html'
 	paginate_by = 40
+	ordering = ['name']
 
 	def get_query(self):
 		cities = City.objects.all()
@@ -31,30 +31,19 @@ class CityListView(ListView):
 		except Exception as e:
 			return HttpResponse(str(e), status=500)
 
+class CitySearchView(View):
+	def get(self, request):
+		form = CitySearchForm()
+		return render(request, 'city_search.html', {'form': form})
 
-def home(request):
-	form = CitySearchForm()
-	return render(request, 'city_search.html', {'form': form})
-
-
-class CitySearchView(FormView):
-	template_name = 'city_search.html'
-	form_class = CitySearchForm
-	success_url = reverse_lazy('city-search')
-
-	def form_valid(self, form):
-		search_term = form.cleaned_data['search_term']
-		print("Search term:", search_term)
-		if search_term:
-			self.results = City.objects.filter(name__startswith=search_term).distinct()
-		return super().form_valid(form)
-
-	def form_invalid(self, form):
-		print("Form errors:", form.errors)
-		return super().form_invalid(form)
-
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		if hasattr(self, 'results'):
-			context['results'] = self.results
-		return context
+	def post(self, request):
+		form = CitySearchForm(request.POST)
+		if form.is_valid():
+			search_term = form.cleaned_data['search_term']
+			print("Search term:", search_term)
+			if search_term:
+				results = City.objects.filter(name__startswith=search_term).distinct()
+				# print o tamnho de result
+				print("Results:", len(results))
+				return render(request, 'city_search.html', {'form': form, 'results': results})
+		return render(request, 'city_search.html', {'form': form})
